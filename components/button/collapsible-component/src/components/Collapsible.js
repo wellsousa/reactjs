cf26a4cbdeef10886d5bc4208dee2ff2029654f3
wellsousa <wellsousa1987@outlook.com>
2021-12-02 15:58:02 -0300

    collapsible-component: finalização do projeto

--- components/button/collapsible-component/src/components/Collapsible.js
+++ components/button/collapsible-component/src/components/Collapsible.js
@@ -1,12 +1,27 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
+
+import './collapsible.css';
 
 function Collapsible(props){
 
     const [isOpen, setIsOpen] = useState(false);
+
+    const parentRef = useRef();
+
     return (
         <div className="collapsible">
-            <button className="toggle" onClick={() => setIsOpen(!isOpen)}>{props.label}</button>
-            {isOpen && <div className="content">{props.children}</div>}
+            <button className="toggle" onClick={() => setIsOpen(!isOpen)}>{isOpen ? "-": props.label}</button>
+            <div className="content-parent" ref={parentRef} style={
+                isOpen ? {
+                    height: parentRef.current.scrollHeight + "px"
+                }:{
+                    height: "0px"
+                }
+            }>
+                <div className="content">
+                    {props.children}
+                </div>
+            </div>
         </div>
     )
 }
